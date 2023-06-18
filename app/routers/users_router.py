88556b75138c72171edc78b83d0f9c8f66d29e83
Camilo Avila <camilo.avil@gmail.com>
2023-06-17 19:48:59 -0500

    Add a simple docString to a function get

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -18,7 +18,19 @@ def get_users(offset: int = Query(description='Offset of the query',default=1,ge
     return users
 
 @users_router.get('/user/{id}', tags=['users'], response_model=User, status_code=status.HTTP_200_OK)
-def get_user(id: int = Path(description='ID of the user to get',example=1,ge=1), session: Session = Depends(get_session)) -> User:
+def get_user(id: int = Path(description='ID of the user to get',example=1,ge=1), 
+             session: Session = Depends(get_session)) -> User:
+    """
+    Retrieves a user by their ID. 
+    
+    Args:
+        id (int): ID of the user to retrieve. Must be greater than or equal to 1.
+        session (Session): A SQLAlchemy Database Session dependency.
+
+    Returns:
+        User: A Pydantic User model representing the retrieved user.
+        JSONResponse (status_code=404): A JSON response containing an error message if the user is not found.
+    """
     user = session.get(User, id)
     if user is not None:
         return user
