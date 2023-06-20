33f9bdd89cb2beaecc6bd5256213388b813d6b59
Camilo Avila <camilo.avil@gmail.com>
2023-06-19 19:04:04 -0500

    Add File uploading

--- app/routers/files_router.py
+++ app/routers/files_router.py
@@ -6,6 +6,19 @@ files_router = APIRouter()
                    #response_model=ResponseModel, 
                    status_code=status.HTTP_201_CREATED)
 async def upload_file(file: UploadFile = File(...)):
+    """
+    Uploads a file to the server.
+
+    Args:
+        file: An instance of UploadFile containing information about the file to be uploaded.
+
+    Returns:
+        A dictionary containing the filename, format, and size of the uploaded file.
+
+    Raises:
+        HTTPException: If there is an error during file upload.
+
+    """
     return {"filename": file.filename,
             "format": file.content_type,
             "size": file.size}
