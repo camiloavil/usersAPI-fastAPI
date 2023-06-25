4d54a3531ed7e7b67650bb62af7e76af449cb267
Camilo Avila <camilo.avil@gmail.com>
2023-06-25 10:19:28 -0500

    Change Model User and variants

--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,40 @@
+# SQLModel
 from sqlmodel import SQLModel, Field
+# Python
+from uuid import UUID, uuid4
 from typing import Optional
 from datetime import datetime
 
 class UserBase(SQLModel):
     name  : str = Field(min_length=3, max_length=50)
-    email : str = Field(index=True, min_length=10, max_length=50)
-    city  : str = Field(min_length=3, max_length=50)
+    email : str = Field(unique=True, min_length=10, max_length=50)
 
 class User(UserBase, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    initDate : Optional[datetime] = None
+    user_id : UUID = Field(default_factory=uuid4,
+                           primary_key=True,
+                           index=True,
+                           nullable=False)
+    pass_hash : str
+    initDate : datetime = Field(default_factory=datetime.now)
+    userType : str = Field(default='user')
+    city : Optional[str] = None
+    idTelegram : Optional[int] = None
+    # city  : str = Field(min_length=3, max_length=50)
+    # id: Optional[int] = Field(default=None, primary_key=True)
 
-class UserRead(UserBase):
-    id: int
-    initDate: datetime
+class UserFB(UserBase):
+    user_id : UUID
+    initDate : datetime
+    city : Optional[str] = None
+    idTelegram : Optional[int] = None
+    class Config:
+        schema_extra = {
+            'example': {
+                'name'      : 'Name of User',
+                'email'     : 'email of User',
+                'user_id'   : 'id of User'
+            }
+        }
 
 class UserCreate(UserBase):
     class Config:
