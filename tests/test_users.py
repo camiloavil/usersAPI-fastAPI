a0b22afeca4fed5b05842dc34ef757f149a7bb7a
Camilo Avila <camilo.avil@gmail.com>
2023-07-02 22:01:45 -0500

    Test(User Path operations) set batery test for path operations if user

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,7 +4,7 @@ import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 # APP
-from app.models.user import SQLModel,User, UserBase, UserFB
+from app.models.user import SQLModel,User, UserBase, UserFB, UserUpdate
 from app.security.secureuser import get_password_hash
 from app.DB.db import get_session
 from app.DB.querys import add_user_to_db
@@ -12,7 +12,6 @@ from app.DB.querys import add_user_to_db
 from sqlmodel import Session, create_engine
 # Python
 import os
-import json
 
 from main import app
 
@@ -108,80 +107,58 @@ def test_create_newuser_repeted(client: TestClient):
     response = client.post("/users/newuser",json=user,)
     assert response.status_code == status.HTTP_409_CONFLICT, "Status code not 409 Conflict"
 
-def test_check_login_of_user_check_his_info(client: TestClient, setUp_users: setUp_users):
+def test_check_login_USER_CRUD(client: TestClient, setUp_users: setUp_users):
     user1,user2,user3 = setUp_users
+    #Test LogIN
     response = client.post("/userlogin",data= {"username": user1['email'], "password": user1['password']})
     assert response.status_code == status.HTTP_200_OK, "Status code not 200 on login"
     assert 'access_token' in response.json(), "Response must have an access_token"
     assert response.json()['token_type'] == 'bearer', "Response must be token_type 'bearer'"
-    header_user = {"Authorization" : "Bearer " + response.json()['access_token']}
+    header_user1 = {"Authorization" : "Bearer " + response.json()['access_token']}
 
-    response = client.get("/users/myuser", headers=header_user)
+    #Test Info GET /users/myuser
+    response = client.get("/users/myuser", headers=header_user1)
     assert response.status_code == status.HTTP_200_OK, "Status code not 200 on path GET /users/myuser"
     assert all(key in response.json() for key in list(UserFB.__annotations__)) , "Response must have all keys of UserFB"
     assert all(key in response.json() for key in list(UserBase.__annotations__)) , "Response must have all keys of UserFB"
     assert (all( (user1[key] == response.json()[key]) for key in list(UserBase.__annotations__))) , "UserFB returned is different"
-    assert user1['name'] == response.json()['name'], "Response must be equal in all fields"
     assert 'initDate' in response.json(), "Response must have an initDate"
 
-#Not necesary, is reduntant
-# def test_create_newuser_(client: TestClient):
-#     params = {
-#         "name": "Testing",
-#         "email": "test@example.com",
-#         "password": "MyPassword"
-#         }
-#     response = client.post("/users/newuser",json=params,)
-#     params.pop('password')  # remove password it never should be returned
-#     params['userType'] = 'free'
-#     assert response.status_code == status.HTTP_201_CREATED, "Status code not 201"
-#     assert all(key in response.json() for key in params) , "Response Incomplete. it hasn't all keys"
-#     assert UserBase(**response.json()) == UserBase(**params), "User created is different" 
-#     assert 'pass_hash' not in response.json() , "pass_hash Should never be in the response"
+    #Test Info PUT /users/myuser
+    #test User1
+    user1_changedata = {"name": "Change User Onw", "email": "testchange@example.com", "password": "otherPassword", "city": "CityChangeOne", "country": "Testland ChangeOne", "userType": "free"}
+    response = client.put("/users/myuser", json=UserUpdate(**user1_changedata).dict(),)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401_UNAUTHORIZED on path PUT /users/myuser whitout headers"
+    response = client.put("/users/myuser", json=user1_changedata , headers=header_user1)
+    assert response.status_code == status.HTTP_200_OK, "Status code not 200 on path PUT /users/myuser"
+    assert response.json()['email'] == user1['email'], "User email must not change"
+    assert response.json()['name'] == user1_changedata['name'], "User name must changed"
+    assert response.json()['city'] == user1_changedata['city'], "User city must changed"
+    assert response.json()['country'] == user1_changedata['country'], "User country must changed"
+    assert response.json()['userType'] == user1['userType'], "User userType must not change"
 
 
-#     def test_info_user_path_without_credentials(self):
-#         # response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
-#         response = client.get("/users/myuser")
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401 on path GET /users/myuser"
+    #Test Info DELETE /users/myuser
+    response = client.post("/userlogin",data= {"username": user2['email'], "password": user2['password']})
+    header_user2 = {"Authorization" : "Bearer " + response.json()['access_token']}
+    response = client.delete("/users/myuser",)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401_UNAUTHORIZED on path DELETE /users/myuser whitout headers"
+    response = client.delete("/users/myuser", headers=header_user2)
+    assert response.status_code == status.HTTP_200_OK, "Status code not 200 OK on path DELETE /users/myuser whit headers"
+    assert 'disabled' in response.json()['message'], "User must be disabled after deletion"
 
-#         response = client.put("/users/myuser")
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401 on path PUT /users/myuser"
-
-#         response = client.delete("/users/myuser")
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401 on path DELETE /users/myuser"
-
-#         response = client.put("/users/myuser/changepassword")
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401 on path PUT /users/myuser/changepassword"
-
-#     def test_login_user_must_be_same_user(self):
-#         # response = client.get("/items/foo", headers={"X-Token": "coneofsilence"})
-#         setnew_dbTesting()
-#         #create users
-#         user1 = {"name": "Uno Name", "email": "uno@example.com", "password": "MyPassword1"}
-#         user2 = {"name": "Dos Name", "email": "dos@example.com", "password": "MyPassword2"}
-#         client.post("/users/newuser",json=user1,)
-#         client.post("/users/newuser",json=user2,)
-#         dataLoginError = {'username': 'value1', 'password': 'value2'}
-#         response = client.post("/userlogin", data=dataLoginError)
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401 on path GET /users/myuser"
-
-#         dataLogin1 = {'grant_type':'',
-#                       'scope':'',
-#                       'client_id':'',
-#                       'client_secret':'',
-#                       'username': user1["email"], 'password': user1["password"]}
-        
-#         dataLogin = {'username': user1["email"], 'password': user1["password"]}
-#         print(str(dataLogin))
-#         responseToken = client.post("/userlogin", 
-#                                     headers={"Accept": "application/json",
-#                                              "Content-Type": "application/x-www-form-urlencoded"}, 
-#                                     data=dataLogin)
-#                                     # json=dataLogin1)
-#         print(str(responseToken.json()))
-#         assert response.status_code == status.HTTP_200_OK, "Status code must be 200"
-#         assert 'access_token' in responseToken.json(), "access_token must be in Correct Response"
-#         # tokeUser1 = responseToken.json()['access_token']
-
-        
+    #Test Info CHANGE PASSWORD /users/myuser
+    new_password_User2 = "newPassword_User2"
+    response = client.put("/users/myuser/changepassword",)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401_UNAUTHORIZED on path PUT /users/myuser/changepassword whitout headers"
+    data_password = {"actual_password": new_password_User2, "new_password": new_password_User2}
+    response = client.put("/users/myuser/changepassword", data = data_password ,headers=header_user2)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 200 OK on path PUT /users/myuser/changepassword whit headers Error Actual password"
+    data_password = {"actual_password": user2['password'], "new_password": new_password_User2}
+    response = client.put("/users/myuser/changepassword", data = data_password ,headers=header_user2)
+    assert response.status_code == status.HTTP_200_OK, "Status code not 200 OK on path PUT /users/myuser/changepassword whit headers Error Actual password"
+    response = client.post("/userlogin",data= {"username": user2['email'], "password": user2['password']})
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, "Status code not 401_UNAUTHORIZED on path LOGIN whit old password"
+    response = client.post("/userlogin",data= {"username": user2['email'], "password": new_password_User2})
+    assert response.status_code == status.HTTP_200_OK, "Status code not 200 OK on path on  LOGIN whit new password"
+    
